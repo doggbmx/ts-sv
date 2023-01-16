3d35456ea49e7117d6ac8c820f6f9a7ad6925975
doggbmx <manuelorticosta@gmail.com>
2023-01-16 10:18:17 -0300

    adds .env file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('hello');
 })
 
-app.listen(8080, () => console.log(`toy escuchando 8080`));
+app.listen(PORT, () => console.log(`toy escuchando ${PORT}`));
