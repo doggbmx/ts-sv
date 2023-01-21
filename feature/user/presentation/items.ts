b6a9af26c29a70fb330ba8244d538277e36cbcf3
doggbmx <manuelorticosta@gmail.com>
2023-01-21 19:57:25 -0300

    configs users router

--- feature/user/presentation/items.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from "express";
-
-const router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-    res.send({data: 'chupame los huevos'})
-});
-
-export { router };
---
+++ feature/user/presentation/user_router.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import { UserRepositories } from '../domain/repositories/user_repositories';
+
+type UserRequestQuery = { userName?: string };
+
+export default function usersRouter(usersRepository: UserRepositories) {
+    const router = express.Router();
+
+    router.get('/', async (req: Request, res: Response) => {
+        try {
+            const { userName } = req.query as UserRequestQuery;
+            const users = await usersRepository.getUser();
+        } catch (error) {
+            throw error;
+        }
+    });
+}
