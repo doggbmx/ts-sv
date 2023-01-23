8341391d83938de78729734886227edc89436273
doggbmx <manuelorticosta@gmail.com>
2023-01-23 10:50:36 -0300

    config routing on users

--- feature/user/presentation/index.ts
+++ feature/user/presentation/index.ts
@@ -1,8 +1,10 @@
 import { pool } from '../../../src/core/services/database_service';
 import { PGUsersDataSource } from '../data/data_source/pg_data_source';
 import { UserRepositoriesImplementation } from '../domain/repositories/user_repository_implementation';
-import usersRouter from './user_router';
+import UsersRouter from './user_router';
 
 const usersDataSource = PGUsersDataSource.create(pool);
 
-const usersRepository = UserRepositoriesImplementation.create
+const usersRepository = UserRepositoriesImplementation.create(usersDataSource);
+
+export const usersRouter = UsersRouter(usersRepository);
