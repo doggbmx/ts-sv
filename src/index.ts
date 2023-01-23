4556c7eac5416ffa3cb5702deb3dd8578ddfdbba
doggbmx <manuelorticosta@gmail.com>
2023-01-23 10:49:57 -0300

    config router on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import { configureRouting } from './core/routing';
 
 const app = express();
+
+app.use(express.json());
+// config del router
+configureRouting(app);
 
 app.listen(4200, () => {
     console.log('corriendo en el 4200');
