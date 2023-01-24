e8ec62d6c76ab839c0fd597d61c9277d77e894f3
doggbmx <manuelorticosta@gmail.com>
2023-01-24 10:16:36 -0300

    fixed typos from users

--- src/feature/user/data/query_scripts/queries.ts
+++ src/feature/user/data/query_scripts/queries.ts
@@ -1,3 +1,3 @@
 export const SELECT_USERS_QUERY = `SELECT * FROM public.user`
 
-export const SELECT_USER_QUERY = `SELECT * FROM public.user WHERE user.id = $1`
+export const SELECT_USER_QUERY = `SELECT * FROM public.user WHERE id = $1`
