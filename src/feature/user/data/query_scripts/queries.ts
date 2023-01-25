7b36fb3eb87f6fc1edf3429f5e1a6643d401a45e
doggbmx <manuelorticosta@gmail.com>
2023-01-25 10:50:39 -0300

    add the insert user query

--- src/feature/user/data/query_scripts/queries.ts
+++ src/feature/user/data/query_scripts/queries.ts
@@ -1,3 +1,9 @@
 export const SELECT_USERS_QUERY = `SELECT * FROM public.user`
 
 export const SELECT_USER_QUERY = `SELECT * FROM public.user WHERE id = $1`
+
+export const INSERT_USER_QUERY = `
+INSERT INTO public.user (name, email, password, user_type)
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+`;
