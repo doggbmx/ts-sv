bbbdcb6355c81f10c2b0b62a3d8e512b1b1d735e
doggbmx <manuelorticosta@gmail.com>
2023-01-27 14:23:42 -0300

    add the updateUser middleware

--- src/feature/user/presentation/users_middleware.ts
+++ src/feature/user/presentation/users_middleware.ts
@@ -19,9 +19,17 @@ const validateBodyParam = (value: string, optional = false) => {
 };
 
 export const getUserValidator = [validateId()];
+
 export const createUserValidator = [
     validateBodyParam("name").isLength({ min:3 }).withMessage("Invalid name"),
     validateBodyParam("email").isEmail().isLength({ min:3 }).withMessage("Invalid Email"),
     validateBodyParam("password").isLength({ min:8 }).withMessage("Password too short!"),
     validateBodyParam("userType").isLength({ min:3 }).withMessage("Invalid sport!")
+];
+
+export const updateUserValidator = [
+    validateBodyParam("name", true).isLength({min:3}).withMessage("Invalid name"),
+    validateBodyParam("email", true).isEmail().isLength({min:3}).withMessage("Invalid Email"),
+    validateBodyParam("password", true).isLength({min:8}).withMessage("Password too short!"),
+    validateBodyParam("userType", true).isLength({min:3}).withMessage("Invalid sport!")
 ];
