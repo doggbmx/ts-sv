bcd3ec5b5b9778df048727e0efad1a65242e37d5
doggbmx <manuelorticosta@gmail.com>
2023-02-10 17:33:39 -0300

    add query to get the user by query

--- src/feature/user/data/query_scripts/queries.ts
+++ src/feature/user/data/query_scripts/queries.ts
@@ -1,4 +1,4 @@
-export const SELECT_USERS_QUERY = `SELECT * FROM public.user;`;
+export const SELECT_USERS_QUERY = `SELECT * FROM user;`;
 
 export const SELECT_USER_QUERY = `SELECT * FROM public.user WHERE id = $1;`;
 
@@ -12,4 +12,10 @@ export const UPDATE_USER_QUERY = `
 UPDATE public.user
 SET name=$2, email=$3, password=$4, user_type=$5
 WHERE id=$1 RETURNING *;
+`;
+
+export const SELECT_USER_WITH_TECH = `
+SELECT * FROM public.tech t
+INNER JOIN user_techs ut
+ON t.id = ut.tech_id AND ut.user_id = $1;
 `;
