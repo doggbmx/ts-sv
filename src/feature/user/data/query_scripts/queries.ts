82942b722b3c0e73d74ca3947595d9d298fe61a0
doggbmx <manuelorticosta@gmail.com>
2023-02-13 11:21:57 -0300

    add query to insert user tech & fixed typos of structure

--- src/feature/user/data/query_scripts/queries.ts
+++ src/feature/user/data/query_scripts/queries.ts
@@ -1,16 +1,16 @@
-export const SELECT_USERS_QUERY = `SELECT * FROM user;`;
+export const SELECT_USERS_QUERY = `SELECT * FROM public.user;`;
 
 export const SELECT_USER_QUERY = `SELECT * FROM public.user WHERE id = $1;`;
 
 export const INSERT_USER_QUERY = `
-INSERT INTO public.user (name, email, password, user_type)
-VALUES ($1, $2, $3, $4)
+INSERT INTO public.user (username, email, password)
+VALUES ($1, $2, $3)
 RETURNING *;
 `;
 
 export const UPDATE_USER_QUERY = `
 UPDATE public.user
-SET name=$2, email=$3, password=$4, user_type=$5
+SET username=$2, email=$3, password=$4, user_type=$5
 WHERE id=$1 RETURNING *;
 `;
 
@@ -18,4 +18,10 @@ export const SELECT_USER_WITH_TECH = `
 SELECT * FROM public.tech t
 INNER JOIN user_techs ut
 ON t.id = ut.tech_id AND ut.user_id = $1;
+`;
+
+export const INSERT_USER_TECH = `
+INSERT INTO public.user_techs (user_id, tech_id)
+VALUES ($1, $2)
+RETURNING *;
 `;
