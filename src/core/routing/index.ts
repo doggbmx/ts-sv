f8653f6bf51ca88da3c4b0d7760eb940c197ed8e
doggbmx <manuelorticosta@gmail.com>
2023-03-14 13:01:53 -0300

    add '/auth' route to router

--- src/core/routing/index.ts
+++ src/core/routing/index.ts
@@ -1,9 +1,11 @@
 import express, { Application } from 'express';
 import { usersRouter } from '../../feature/user/presentation';
+import { authRouter } from '../../feature/auth/presentation';
 
 
 export const configureRouting = (app: Application) => {
     const router = express.Router();
     app.use('/api', router);
+    router.use('/auth', authRouter);
     router.use('/users', usersRouter);
 };
