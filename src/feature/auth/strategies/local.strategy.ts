d3907941b7efdec1f6700a45061e8f7c415379bf
doggbmx <manuelorticosta@gmail.com>
2023-03-14 13:02:28 -0300

    adds local strategy

--- src/feature/auth/strategies/local.strategy.ts
+++ src/feature/auth/strategies/local.strategy.ts
@@ -2,9 +2,9 @@ import { Strategy as LocalStrategy } from 'passport-local';
 import { usersRepository } from '../../user/presentation/index';
 import bcrypt from 'bcrypt' 
 
-export const localStrategy = new LocalStrategy(async (username, password, done) => {
+export const localStrategy = new LocalStrategy({usernameField: 'email'},async (email, password, done) => {
     try {
-        const user = await usersRepository.getUserByEmail(username);
+        const user = await usersRepository.getUserByEmail(email);
         if (!user) {
             done(new Error, false);
         }
