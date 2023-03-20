65daa8af8004e1bcaf9deeed36824ad98a69be7d
doggbmx <manuelorticosta@gmail.com>
2023-03-20 13:15:32 -0300

    add jwt strategy to passport

--- src/feature/auth/index.ts
+++ src/feature/auth/index.ts
@@ -1,4 +1,6 @@
 import passport from 'passport'
 import { localStrategy } from './strategies/local.strategy'
+import { jwtStrategy } from './strategies/jwt.strategy'
 
-passport.use(localStrategy)
+passport.use(localStrategy);
+passport.use(jwtStrategy);
---
+++ src/feature/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,12 @@
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { config } from '../../../core/config/config';
+import { usersRepository } from '../../user/presentation';
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.jwtSecret,
+};
+
+export const jwtStrategy = new Strategy(options, async (payload, done) => {
+   return done(null, payload); 
+});
