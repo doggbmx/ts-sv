6b7bc5c2a758aa376bf9693353f69bc32aafb180
doggbmx <manuelorticosta@gmail.com>
2023-03-20 11:58:15 -0300

    now express uses jwt

--- src/feature/auth/presentation/auth_router.ts
+++ src/feature/auth/presentation/auth_router.ts
@@ -1,7 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { User } from "../../user/domain/models/user_model";
-import { UserRepositories } from "../../user/domain/repositories/user_repositories";
+import jwt ,{ Secret } from 'jsonwebtoken'
+
 import passport from 'passport';
+import { config } from '../../../core/config/config';
 
 export default function AuthRouter() {
     const router = express.Router();
@@ -11,8 +13,17 @@ export default function AuthRouter() {
         async (req: Request, res: Response, next: NextFunction) => {
             try {
                 let user= req.user as Partial<User>;
+                const payload = {
+                    sub: user.userId,
+                };
+                const token = jwt.sign(payload, config.jwtSecret as Secret, {
+                    expiresIn: '1d',
+                });
                 delete user.password;
-                res.json(user);
+                res.json({
+                    user,
+                    token,
+                });
             } catch (error) {
                 next(error);
             }
