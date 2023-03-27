cbb882aafbcad8591f620057daa42cf115484bf2
doggbmx <manuelorticosta@gmail.com>
2023-03-27 11:00:19 -0400

    checkIsManu middleware add

--- src/feature/auth/auth.handler.ts
+++ src/feature/auth/auth.handler.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { User } from "../user/domain/models/user_model";
 
 export function checkApiKey(req: Request, res: Response, next: NextFunction) {
     const apiKey = req.headers['api'];
@@ -8,6 +9,18 @@ export function checkApiKey(req: Request, res: Response, next: NextFunction) {
     } else {
         return res.status(401).send({
             error: 'NOPE BRO'
+        });
+    }
+}
+
+export function checkIsManu(req: Request, res: Response, next: NextFunction) {
+    const user = req.user as User;
+    console.log(user);
+    if (user.name == 'manu') {
+        next();
+    } else {
+        return res.status(403).send({
+            error: 'NO ERES MANU'
         });
     }
 }
