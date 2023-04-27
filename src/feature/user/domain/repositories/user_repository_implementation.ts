6b32015e0861930b9b36299d577c4561a9afd5a3
doggbmx <manuelorticosta@gmail.com>
2023-04-27 09:50:05 -0400

    formatting purposes

--- src/feature/user/domain/repositories/user_repository_implementation.ts
+++ src/feature/user/domain/repositories/user_repository_implementation.ts
@@ -1,77 +1,79 @@
-import { Type } from 'typescript';
-import { CustomError } from '../../../error/custom_error';
-import { GenericError } from '../../../error/generic_error';
-import { User, UpdateUser, CreateUser } from '../models/user_model';
-import { UserRepositories } from './user_repositories';
-import { UserDataSource } from '../../data/interfaces/user_data_source';
+import { CustomError } from "../../../error/custom_error";
+import { GenericError } from "../../../error/generic_error";
+import { User, UpdateUser, CreateUser } from "../models/user_model";
+import { UserRepositories } from "./user_repositories";
+import { UserDataSource } from "../../data/interfaces/user_data_source";
 // import { CustomErrorHandler } from '../../../error/error_handler'
 
 export class UserRepositoriesImplementation implements UserRepositories {
-    private usersDataSource: UserDataSource;
-    private constructor(dataSource: UserDataSource) {
-        this.usersDataSource = dataSource;
-    }
+  private usersDataSource: UserDataSource;
+  private constructor(dataSource: UserDataSource) {
+    this.usersDataSource = dataSource;
+  }
 
-    static instance: UserRepositories | null = null;
+  static instance: UserRepositories | null = null;
 
-    static create(dataSource: UserDataSource) {
-        if (UserRepositoriesImplementation.instance == null) {
-            UserRepositoriesImplementation.instance = new UserRepositoriesImplementation(dataSource);
-        }
-        return UserRepositoriesImplementation.instance;
+  static create(dataSource: UserDataSource) {
+    if (UserRepositoriesImplementation.instance == null) {
+      UserRepositoriesImplementation.instance =
+        new UserRepositoriesImplementation(dataSource);
     }
+    return UserRepositoriesImplementation.instance;
+  }
 
-    async getUser(id?: string): Promise<User[]> {
-        return await this.callDataSource(async () => {
-            if (id) {
-                return await this.usersDataSource.getUser(id);
-            } 
-            return await this.usersDataSource.getAllUsers();
-        });
-    }
+  async getUser(id?: string): Promise<User[]> {
+    return await this.callDataSource(async () => {
+      if (id) {
+        return await this.usersDataSource.getUser(id);
+      }
+      return await this.usersDataSource.getAllUsers();
+    });
+  }
 
-    async getUserByEmail(email: string): Promise<User> {
-        return await this.callDataSource(async () => {
-            return await this.usersDataSource.getUserByEmail(email);
-        });
-    }
+  async getUserByEmail(email: string): Promise<User> {
+    return await this.callDataSource(async () => {
+      return await this.usersDataSource.getUserByEmail(email);
+    });
+  }
 
-    async createUser (data: CreateUser): Promise<User> {
-        return await this.callDataSource(() => this.usersDataSource.createUser(data));
-    }
+  async createUser(data: CreateUser): Promise<User> {
+    return await this.callDataSource(() =>
+      this.usersDataSource.createUser(data)
+    );
+  }
 
-    async updateUser(userId: string, data: UpdateUser): Promise<User> {
-        return await this.callDataSource(async () => {
-            const selectedUser = await this.usersDataSource.getUser(userId);
-            const updatedUser = {
-                ...selectedUser,
-                ...data,
-            };
-            return await this.usersDataSource.updateUser(updatedUser);
-        });
-    }
+  async updateUser(userId: string, data: UpdateUser): Promise<User> {
+    return await this.callDataSource(async () => {
+      const selectedUser = await this.usersDataSource.getUser(userId);
+      const updatedUser = {
+        ...selectedUser,
+        ...data,
+      };
+      return await this.usersDataSource.updateUser(updatedUser);
+    });
+  }
 
-    async getUserWithTech(id: string): Promise<User> {
-        return await this.callDataSource(async () => {
-            return await this.usersDataSource.getUserWithTech(id);
-        });
-    }
+  async getUserWithTech(id: string): Promise<User> {
+    return await this.callDataSource(async () => {
+      return await this.usersDataSource.getUserWithTech(id);
+    });
+  }
 
-    async createUserTech(userId: string, techId: string): Promise<User> {
-        return await this.callDataSource(async () => {
-            return await this.usersDataSource.createUserTech(userId, techId);
-        });
+  async createUserTech(userId: string, techId: string): Promise<User> {
+    return await this.callDataSource(async () => {
+      return await this.usersDataSource.createUserTech(userId, techId);
+    });
+  }
+
+  private async callDataSource<T>(callback: Function): Promise<T> {
+    try {
+      return await callback();
+    } catch (error) {
+      console.log(error);
+      if (error instanceof CustomError) {
+        throw error;
+      }
+      throw new GenericError("Users Repositories error.");
     }
-    
-    private async callDataSource<T>(callback: Function): Promise<T> {
-        try {
-            return await callback();
-        } catch (error) {
-            console.log(error);
-            if (error instanceof CustomError) {
-                throw error;
-            }
-            throw new GenericError('Users Repositories error.');
-        }
-    }
+  }
 }
