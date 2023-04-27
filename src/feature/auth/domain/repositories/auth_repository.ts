b155430ec3c41a5e3deabb0b06400e92b1a48683
doggbmx <manuelorticosta@gmail.com>
2023-04-27 10:44:47 -0400

    auth_repository created

---
+++ src/feature/auth/domain/repositories/auth_repository.ts
@@ -0,0 +1,9 @@
+import { User } from "../../../user/domain/models/user_model";
+import bcrypt from "bcrypt";
+import { JwtPayload } from "../jwt_payload";
+
+export interface AuthRepository {
+  getUser(email: string, password: string): Promise<Partial<User>>;
+  signToken(user: User): JwtPayload;
+  sendMail(userEmail: string): void;
+}
