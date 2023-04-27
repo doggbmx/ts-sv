f7571fd2246a1a65ab751613cc6f19071f03d78d
doggbmx <manuelorticosta@gmail.com>
2023-04-27 10:44:59 -0400

    auth dependency injection

--- src/feature/auth/presentation/index.ts
+++ src/feature/auth/presentation/index.ts
@@ -2,5 +2,10 @@ import { pool } from "../../../core/services/database_service";
 import { PGUsersDataSource } from "../../user/data/data_source/pg_data_source";
 import { UserRepositoriesImplementation } from "../../user/domain/repositories/user_repository_implementation";
 import AuthRouter from "./auth_router";
+import { AuthRepositoryImplementation } from "../domain/repositories/auth_reposityory_implementation";
+import { usersRepository } from "../../user/presentation";
 
-export const authRouter = AuthRouter();
+export const authRepository =
+  AuthRepositoryImplementation.create(usersRepository);
+
+export const authRouter = AuthRouter(authRepository);
