d65f12f01eb3ee6b3148ceb8d92679a88f4ba3ab
doggbmx <manuelorticosta@gmail.com>
2023-04-29 15:15:35 -0400

    nodemailer is now a service

---
+++ src/core/services/mail_service.ts
@@ -0,0 +1,12 @@
+import nodemailer from "nodemailer";
+import { config } from "../config/config";
+
+export const mailService = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: config.nodeMailerEmail,
+    pass: config.nodeMailerPassword,
+  },
+});
