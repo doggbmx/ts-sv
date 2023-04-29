2d2167f6d224c38b4e48b1baa1326266603e5098
doggbmx <manuelorticosta@gmail.com>
2023-04-29 12:08:40 -0400

    nodemailer email & pass added

--- src/core/config/config.ts
+++ src/core/config/config.ts
@@ -1,12 +1,13 @@
-import 'dotenv'
+import "dotenv/config";
 
 export const config = {
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'secret',
-    db: {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 5432,
-    },
-    refreshToken: process.env.REFRESH_SECRET || 'refresh'
-}
-
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "secret",
+  db: {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+  },
+  refreshToken: process.env.REFRESH_SECRET || "refresh",
+  nodeMailerEmail: process.env.NODEMAILER_EMAIL || "email",
+  nodeMailerPassword: process.env.NODEMAILER_PASS || "password",
+};
