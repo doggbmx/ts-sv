e6d2a789f53bbd8355cbc549aaf6c9846de2dd0f
doggbmx <manuelorticosta@gmail.com>
2023-05-04 10:14:22 -0400

    update query changed

--- src/feature/user/data/query_scripts/queries.ts
+++ src/feature/user/data/query_scripts/queries.ts
@@ -2,7 +2,7 @@ export const SELECT_USERS_QUERY = `SELECT * FROM public.user;`;
 
 export const SELECT_USER_QUERY = `SELECT * FROM public.user WHERE id = $1;`;
 
-export const SELECT_USER_BY_EMAIL = `SELECT * FROM public.user WHERE email = $1 LIMIT 1;`
+export const SELECT_USER_BY_EMAIL = `SELECT * FROM public.user WHERE email = $1 LIMIT 1;`;
 
 export const INSERT_USER_QUERY = `
 INSERT INTO public.user (username, email, password)
@@ -12,7 +12,7 @@ RETURNING *;
 
 export const UPDATE_USER_QUERY = `
 UPDATE public.user
-SET username=$2, email=$3, password=$4, user_type=$5
+SET username=$2, email=$3, password=$4, recovery_token=$5
 WHERE id=$1 RETURNING *;
 `;
 
