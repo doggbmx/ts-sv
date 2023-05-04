ea5d4f124a5205c62487434595515dc936065de4
doggbmx <manuelorticosta@gmail.com>
2023-05-04 12:35:05 -0400

    small fix on user repository

--- src/feature/user/domain/repositories/user_repositories.ts
+++ src/feature/user/domain/repositories/user_repositories.ts
@@ -1,11 +1,11 @@
-
-import { CreateUser, UpdateUser, User } from '../models/user_model';
+import { CreateUser, UpdateUser, User } from "../models/user_model";
 
 export interface UserRepositories {
-    getUser(id?: string): Promise<User[]>;
-    getUserByEmail(email: string): Promise<User>;
-    createUser(data: CreateUser): Promise<User>;
-    updateUser(userId: string, data:UpdateUser): Promise<User>;
-    getUserWithTech(id: string): Promise<User>;
-    createUserTech(userId: string, techId: string): Promise<User>
-};
+  getUser(id?: string): Promise<User>;
+  getAllUsers(): Promise<User[]>;
+  getUserByEmail(email: string): Promise<User>;
+  createUser(data: CreateUser): Promise<User>;
+  updateUser(userId: string, data: UpdateUser): Promise<User>;
+  getUserWithTech(id: string): Promise<User>;
+  createUserTech(userId: string, techId: string): Promise<User>;
+}
