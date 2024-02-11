9b52113b903767cac6cbb930e2bfc7396ed4ecc8
doggbmx <manuelorticosta@gmail.com>
2024-02-11 15:04:59 -0300

    Update database configuration in config.ts

--- src/core/config/config.ts
+++ src/core/config/config.ts
@@ -1,14 +1,17 @@
 import "dotenv/config";
 
 export const config = {
-  port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || "secret",
-  db: {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 5432,
-  },
-  refreshToken: process.env.REFRESH_SECRET || "refresh",
-  smtpEmail: process.env.SMTP_EMAIL || "email",
-  smtpPassword: process.env.SMTP_PASS || "password",
-  smtpHost: process.env.SMTP_HOST || "smtp.gmail.com",
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || "secret",
+    db: {
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 5432,
+        user: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || "root",
+        name: process.env.DB_NAME || "db",
+    },
+    refreshToken: process.env.REFRESH_SECRET || "refresh",
+    smtpEmail: process.env.SMTP_EMAIL || "email",
+    smtpPassword: process.env.SMTP_PASS || "password",
+    smtpHost: process.env.SMTP_HOST || "smtp.gmail.com",
 };
